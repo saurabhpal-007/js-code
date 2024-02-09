4982dba0fe8ef2ab9186472c571acab5f7a186c8
saurabhpal-007 <92792297+saurabhpal-007@users.noreply.github.com>
2024-02-09 18:20:47 +0000

    datatype

---
+++ .devcontainer/basics/datatype.js
@@ -0,0 +1,19 @@
+"use strict";
+console.log(3+3)
+
+let name="saurabh"
+let age=18
+let islogged=false
+let state = null
+
+/*
+number =2 to power 53
+bigint
+string
+boolean true false
+null= standalone value
+undefine= 
+symble =unique
+
+*/
+console.log(typeof age);
